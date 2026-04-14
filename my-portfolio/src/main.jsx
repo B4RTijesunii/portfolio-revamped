2be464a863dc4d80e71d0582bdad2c97e63a1f34
B4RTijesunii <olayodebanke88@gmail.com>
2026-04-14 19:34:39 +0100

    added google console verification

--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { HelmetProvider } from "react-helmet-async";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <meta
+        name="google-site-verification"
+        content="google-site-verification=PcAu7_RHTsgKfUJKc_vmeLymeYPYYLpYCb8rUMYewRA"
+      />
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
 );
 console.log("MAIN jsx IS RUNNIMG");
