408aec267801a5a39cc831bf8bf21bbf038772e2
B4RTijesunii <olayodebanke88@gmail.com>
2026-07-31 18:04:47 +0100

    fixed an error

--- my-portfolio/src/pages/Allprojects.jsx
+++ my-portfolio/src/pages/Allprojects.jsx
@@ -6,7 +6,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Projects from "../components/Projects";
 
-export default function AllProjects() {
+export default function Allprojects() {
   const navigate = useNavigate();
 
   return (
